fb3d19b19f2ca92bd923ac3ed8dc8cbb363b2640
Leonardo Ferrisi <biggestresurgence1492@gmail.com>
2023-04-22 16:24:08 -0400

    added evaluation code, added master file

--- drone_control.py
+++ drone_control.py
@@ -1,29 +1,64 @@
 from djitellopy import Tello
+import time
+
+import keyboard
+    
 
 
-
+state = None
 
 def control_drone():
+    """
+    Control Drone using 
+
+        q w e
+        a s d
+    """
 
     tello = Tello()
-    
-    input("Press enter to connect to Tello Drone")
-
     tello.connect()
 
+    input("Start drone?")
 
-    while True:
+    tello.takeoff()
+    keep_running = True
 
-        command = input("Input a command ( 'u' for UP, anything else for LAND )")
+    print("\n\n========\n\n")
+    print("Drone is running")
 
-        if command != 'u':
-            tello.land()
+    while keep_running:        
+        if keyboard.is_pressed('w') and state != "FWD":
+            print("FWD")
+            tello.move_forward(100)
+            state = "FWD"
+        elif keyboard.is_pressed('a') and state != "LEFT":
+            print("LEFT")
+            tello.move_left(100)
+            state = "LEFT"
+        elif keyboard.is_pressed('s') and state != "BKWD":
+            print("BKWD")
+            tello.move_back(100)
+            state = "BKWD"
+        elif keyboard.is_pressed('d') and state != "RIGHT":
+            print("RIGHT")
+            tello.move_right(100)
+            state = "RIGHT"
+        elif keyboard.is_pressed('q') and state != "RL":
+            print("R-Left")
+            tello.rotate_counter_clockwise(100)
+            state = "RL"
+        elif keyboard.is_pressed('e') and state != "RR":
+            print("R-Right")
+            tello.rotate_clockwise(100)
+            state = "RR"
+        elif keyboard.is_pressed('x'):
+            keep_running = False
+        else: 
+            tello.set_speed(0)
+            continue
+        time.sleep(0.5)
 
-        else:
-            tello.takeoff()
-
-
-
+    tello.land()
 
 if __name__ == "__main__":
     control_drone()
---
+++ master.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+
+
+def start_program(filename):
+    current_dir = os.getcwd()
+    program_path = os.path.join(current_dir, filename)  
+
+
+    # Define the command to open a new command prompt window and run a Python script
+    command = f"start cmd /k python {program_path}"
+
+    # Open a new command prompt window and run the Python script
+    subprocess.call(command, shell=True)
+
+if __name__ == "__main__":
+
+    start_program("startDrone.py")
+    start_program("startEEG.py")
+    start_program("startUI.py")
+    
+
+
+    
